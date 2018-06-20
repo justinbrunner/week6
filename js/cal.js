dd7071791071c8de51eb5141c3ea8bfd7828889a
Brunner <jbrunner@tss-hr420-dmpd.fanshawec.ca>
2018-06-20 13:43:17 -0400

    added in vis for + and -, added in vars for both

--- js/cal.js
+++ js/cal.js
@@ -3,14 +3,18 @@
 	console.log("JS file loaded");
 
 	//Vars
-var num1 = document.querySelector("#input1");
+var num1 = document.querySelector("input");
 var num2 = document.querySelector("#input2");
 var equals = document.querySelector("#trigger");
 var outcome = document.querySelector("#value");
+var selPlus = document.querySelector("#plus");
+var selMinus = document.querySelector("#minus");
 //console.log(num1);
 //console.log(num2);
 //console.log(equals);
 //console.log(outcome);
+//console.log(selPlus);
+//console.log(selMinus);
 
 	//Functions
 	function addStuff() {
