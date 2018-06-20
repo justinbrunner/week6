2c26d2a2a5afce80afeeb7a624102beb4e1cb7c7
Brunner <jbrunner@tss-hr420-dmpd.fanshawec.ca>
2018-06-20 14:35:04 -0400

    finished off conditions for user interaction

--- js/cal.js
+++ js/cal.js
@@ -9,6 +9,7 @@ var equals = document.querySelector("#trigger");
 var outcome = document.querySelector("#value");
 var selPlus = document.querySelector("#plus");
 var selMinus = document.querySelector("#minus");
+var userChoice = "";
 //console.log(num1);
 //console.log(num2);
 //console.log(equals);
@@ -17,19 +18,45 @@ var selMinus = document.querySelector("#minus");
 //console.log(selMinus);
 
 	//Functions
-	function addStuff() {
+	function doStuff() {
+		var total = "";
+		console.log(userChoice);
 		//console.log("from addStuff");
 		//console.log(num1.value);
 		//console.log(num2.value);
-var total = parseInt(num1.value) + parseInt(num2.value);
+		if(userChoice == "add"){
+			total = parseInt(num1.value) + parseInt(num2.value);
+		}else{
+			total = parseInt(num1.value) - parseInt(num2.value);
+		}
 		//console.log(total);
 		outcome.innerHTML = total;
 	}
 
+	function selCal(evt) {
+		//console.log("From selCal");
+		/*if(selPlus.innerHTML == "+") {
+			console.log("User clicked +");
+		}
+
+		if(selMinus.innerHTML == "-"){
+			console.log("User clicked -");
+		}*/
+		//console.log(evt.currentTarget.id);
+		if(evt.currentTarget.id == "plus"){
+			console.log("add");
+			userChoice = "add";
+		}else{
+			console.log("sub");
+			userChoice = "sub";
+		}
+	}
+
 
 	//Listeners
-	equals.addEventListener("click", addStuff, false);
-
+	equals.addEventListener("click", doStuff, false);
+	selPlus.addEventListener("click", selCal, false);
+	selMinus.addEventListener("click", selCal, false);
 
 
 })();
